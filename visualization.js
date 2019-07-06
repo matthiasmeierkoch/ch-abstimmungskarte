e2be8fce7590085c15e532a8a1fd719490a02055
Matthias Koch <matthiasmeierkoch@hotmail.com>
2019-07-06 15:35:04 +0200

    start

--- visualization.js
+++ visualization.js
@@ -5,12 +5,41 @@ document.addEventListener("DOMContentLoaded", () => {
       d3.csv("/referendum.csv")
         .then(yesVotes => {
 
-          // ====================
+            const width = 800;
+            const height = 800;
 
-          // Visualisierung...
+            const container = d3.select("#viz");
 
-          // ====================
+            const svg = container.append("svg")
+                .attr("width", width)
+                .attr("height", height)
+
+            const projection = d3.geoAlbers()
+                .center([0, 46.7])
+                .rotate([-9, 0, 0])
+                .parallels([40, 50])
+                .scale(12500)
+
+            const pathGenerator = d3.geoPath().projection(projection);
+
+            const colorScale = d3.scaleThreshold()
+                .domain([30,35,40,45,50,55,60,65,70,100])
+                .range(["#d0001b", "#e0513c", "#ee7e5f", "#f7a684", "#fdceaa", "#d0e0af", "#a6c185", "#7da35b", "#538633", "#256900"])
+
+            const switzerland = svg.selectAll("path")
+                .data(cantons.features)
+                .enter()
+                .append("path")
+                .attr("d", pathGenerator)
+                .attr("fill", "#DDD" )
+                .attr("stroke", "white")
+                .attr("fill", function (d) {
+                    const voteMetaData = yesVotes.find(yesVote => yesVote.id == d.properties.id)
+
+                    return colorScale(voteMetaData.ja_anteil)
+                })
+            })
 
         })
     })
-})
+
